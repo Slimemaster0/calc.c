27f629df18de90e6ac4563f2295ce7a4da80c59a
Slimemaster <milo@munksgaard.eu>
2023-02-05 14:05:26 +0100

    added "cd" command to the shell

--- sh.c
+++ sh.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+char changeDir(char cmd[100]) {
+    char command[100] = "";
+    strcpy(command, cmd);
+    strtok(command, " ");
+    
+    
+
+    if (strcmp(command, "cd") == 0) {
+	char *pathPtr = command;
+	pathPtr += 3;
+	char path[97];
+	strcpy(path, pathPtr);
+	strtok(path, "\n");
+
+	int success = chdir(path);
+	char s[100];
+	setenv("PWD", getcwd(s, 100), 1);
+
+	return 0;
+    } else {
+	return 1;
+    }
+}
 
 int main() {
     while (0 == 0) {
+	char s[100];
 	char command[100] = "";
 
-	char *pwd = getenv("PWD");
-	printf("%s $ ", pwd);
+	printf("%s $ ", getcwd(s, 100));
 	fgets(command, sizeof(command), stdin);
 
 	if (strcmp(command, "exit\n") == 0) { break; }
+	if (changeDir(command) == 0) { continue; }
 
 	system(command);
     }
