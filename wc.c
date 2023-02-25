3e3276743ed817d964518ec7eab119ff6ace2082
Slimemaster <milo@munksgaard.eu>
2023-02-25 17:14:16 +0100

    improved word count

--- wc.c
+++ wc.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 #include "colors.h"
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    char mode = 0;
+    char ignoreLine = 0;
+
     for (int i = 1; i < argc; i++) {
 	if (strcmp(argv[i], "-c") == 0) {
-	    printf("%sERR:%s the argument \"-c\" is not implemented yet", ANSI_COLOR_RED, ANSI_COLOR_RESET);
+	    mode = 1;
+	    ignoreLine = i;
 	    break;
 	} else if (strcmp(argv[i], "-l") == 0) {
-	    printf("%sERR:%s the argument \"-l\" is not implemented yet", ANSI_COLOR_RED, ANSI_COLOR_RESET);
+	    mode = 2;
+	    ignoreLine = i;
 	    break;
 	} else if (strcmp(argv[i], "-w") == 0) {
-	    printf("%sERR:%s the argument \"-w\" is not implemented yet", ANSI_COLOR_RED, ANSI_COLOR_RESET);
+	    mode = 3;
+	    ignoreLine = i;
 	    break;
 	}
     }
@@ -19,34 +26,55 @@ int main(int argc, char *argv[]) {
     FILE *fptr;
 
     for (int i = 1; argc > i; i++) {
+	if (ignoreLine == i) { continue; }
+
 	fptr = fopen(argv[i], "r");
 
-	fseek(fptr, 0l, SEEK_END); // Preparation for getting the size of the file.
-	size_t len = ftell(fptr); // get the length of the input file.
-	fclose(fptr);
-	fptr = fopen(argv[i], "r");
+	size_t len;
+	if (mode == 0 | mode == 1) {
+	    fseek(fptr, 0l, SEEK_END); // Preparation for getting the size of the file.
+	    len = ftell(fptr); // get the length of the input file.
+	    fclose(fptr);
+	    fptr = fopen(argv[i], "r");
+	}
 	
-	char ch;
 	int lineCount = 0l;
-	while (ch != EOF) {
-	    ch = fgetc(fptr);
-	    if (ch == '\n') {
-		lineCount++;
+	char ch;
+	if (mode != 1) {
+	    while (ch != EOF) {
+		ch = fgetc(fptr);
+		if (ch == '\n') {
+		    lineCount++;
+		}
 	    }
+	    fclose(fptr);
+	    fptr = fopen(argv[i], "r");
 	}
-	fclose(fptr);
-	fptr = fopen(argv[i], "r");
 
-	ch = 69;
 	int wordCount = lineCount;
-	while (ch != EOF) {
-	    ch = fgetc(fptr);
-	    if (ch == ' ') {
-		wordCount++;
+	ch = 69;
+	if (mode == 0 | mode == 3) {
+	    while (ch != EOF) {
+		ch = fgetc(fptr);
+		if (ch == ' ') {
+		    wordCount++;
+		}
 	    }
 	}
 
-	printf("Charactor count: %ld\nLine count: %d\nWord count: %d\n", len, lineCount, wordCount);
+	switch (mode) {
+	    case 0: 
+		printf("Charactor count: %ld\nLine count: %d\nWord count: %d\n", len, lineCount, wordCount);
+		break;
+	    case 1:
+		printf("%ld\n", len);
+		break;
+	    case 2: 
+		printf("%d\n", lineCount);
+		break;
+	    case 3:
+		printf("%d\n", wordCount);
+	}
 	fclose(fptr);
     }
     
