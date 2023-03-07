166d6bbd1d95405669cc5fe0713653f3ad3b2691
Slimemaster <milo@munksgaard.eu>
2023-03-07 16:56:15 +0100

    Allow a directory to be specified with ls

--- ls.c
+++ ls.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
-#include <unistd.h>
 #include "colors.h"
 
 #define BUF_SIZE 256
 
 int main(int argc, char *argv[]) {
+    char path[BUF_SIZE] = ".";
+
     char showHidden = 0;
     for (int i = 1; i < argc; i++) {
 	if (strcmp(argv[i], "-a") == 0) {
 	    showHidden = 1;
+	    continue;
 	}
+	strcpy(path, argv[i]);
     }
 
-    struct dirent *pDirent;
-    char path[BUF_SIZE] = "\0";
-
-    if (path[0] == '\0') {
-	getcwd(path, BUF_SIZE);
-    }
+    struct dirent *pDirent; // Directory ending.
 
     DIR *dirP = opendir(path);
     if (dirP == NULL) { // test if the directory exists
